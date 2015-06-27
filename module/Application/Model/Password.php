89e30dae31b05576a0e504201cee508a0fd4e086
Roshan <roshanbhgt@gmail.com>
2015-06-27 23:05:49 +0530

    Added the url generator and password encryptor functional files
    Signed-off-by: Roshan <roshanbhgt@gmail.com>

---
+++ module/Application/Model/Password.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Application\Model;
+use Zend\Crypt\Password\Bcrypt;
+
+class Password
+{
+
+    public $salt = 'bdbvbhbvmbbcvncvnc515131';
+
+    public $method = 'md5';
+
+    /**
+     * Constructor
+     *
+     * @author Arvind Singh
+     * @access public
+     *        
+     * @param string $method
+     *            // Encryption method
+     * @return void
+     */
+    public function __construct($method = null)
+    {
+        if (! is_null($method)) {
+            $this->method = $method;
+        }
+    }
+
+    /**
+     * Create Password
+     *
+     * @author Arvind Singh
+     * @access public
+     *        
+     * @param string $password
+     *            User Password
+     * @return string
+     */
+    public function create($password)
+    {
+        if ($this->method == 'md5') {
+            return md5($this->salt . $password);
+        } elseif ($this->method == 'sha1') {
+            return sha1($this->salt . $password);
+        } elseif ($this->method == 'bcrypt') {
+            $bcrypt = new Bcrypt();
+            $bcrypt->setCost(14);
+            return $bcrypt->create($password);
+        }
+    }
+
+    /**
+     * Validate the user password
+     *
+     * @author Arvind Singh
+     * @access public
+     *        
+     * @param string $password
+     *            // Password string
+     *            
+     * @param string $hash
+     *            // Hash string
+     *            
+     * @return boolean
+     */
+    public function verify($password)
+    {
+        if ($this->method == 'md5') {
+            return md5($this->salt . $password);
+        } elseif ($this->method == 'sha1') {
+            return sha1($this->salt . $password);
+        } elseif ($this->method == 'bcrypt') {
+            $bcrypt = new Bcrypt();
+            $bcrypt->setCost(14);
+            return $bcrypt->verify($password, $hash);
+        }
+    }
+}
